28f2e1ab7cf8c18a44c51898c63b4d75dc890f59
TinnyDatta <tinnydatta.prof@gmail.com>
2024-06-02 15:05:15 +0600

    added basic features section

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   <div className="hero-content  flex-col lg:flex-row-reverse">
     <img  src="/images/fitnessBanner.jpg" className="w-[300px] h-[300px] rounded-lg shadow-2xl" />
     <div className="mr-8">
-      <p className="text-[#CD5C5C] my-4">Breathe, Relax and br Healthy</p>
+      <p className="text-[#CD5C5C] my-4 text-xl">Breathe, Relax and br Healthy</p>
       <h1 className="text-5xl text-[#8A3324] ">This is FitnessBlender center</h1>
       <p className="py-6">By joining with us you can stay fit by various physical and mental classes.</p>
       <button className="btn bg-[#CD5C5C] text-white">Get Started</button>
---
+++ src/components/Home/Features/Features.jsx
@@ -0,0 +1,83 @@
+
+import { MdOutlinePersonalInjury } from "react-icons/md";
+
+const Features = () => {
+    return (
+        <div className="my-10 ml-20">
+            <h2 className="text-[#CD5C5C] text-center my-2 text-xl">Features</h2>
+            <p className="text-4xl text-[#8A3324] text-center">Services you will get </p>
+            <p className="text-center text-lg mt-3 mb-10">These are the main features of FitnessBlender but there are also a vast services <br /> these can take anyone from anywhere. </p>
+            {/* cards */}
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Personal Training</h2>
+			<p className="text-lg text-gray-500">Tailored workouts with expert guidance for optimal results.</p>
+		</div>
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Group Classes</h2>
+			<p className="text-lg text-gray-500">Dynamic fitness sessions for all skill levels and interests.</p>
+		</div>
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Nutritional Counseling</h2>
+			<p className="text-lg text-gray-500">Personalized diet plans to complement your fitness goals.</p>
+		</div>
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Wellness Programs</h2>
+			<p className="text-lg text-gray-500">Comprehensive plans for overall health and well-being.</p>
+		</div>
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Fitness Assessments</h2>
+			<p className="text-lg text-gray-500">Detailed evaluations to track and improve performance.</p>
+		</div>
+	</div>
+</div>
+            <div className="max-w-xs rounded-md shadow-md">
+	<div>
+    < MdOutlinePersonalInjury className="text-8xl ml-24 text-[#CD5C5C]"/>
+    </div>
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-2xl">Spa Services</h2>
+			<p className="text-lg text-gray-500">Relaxing treatments to aid recovery and rejuvenation.</p>
+		</div>
+	</div>
+</div>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
