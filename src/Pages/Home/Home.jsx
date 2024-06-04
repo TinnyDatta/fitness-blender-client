e54df8a67a5f8bd0261a8845f35ad4d184f851b1
TinnyDatta <tinnydatta.prof@gmail.com>
2024-06-04 09:42:12 +0600

    added team section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../components/Home/Banner/Banner";
 import Features from "../../components/Home/Features/Features";
 import NewsLetter from "../../components/Home/NewsLetter/NewsLetter";
 import Reviews from "../../components/Home/Reviews/Reviews";
+import TeamSection from "../../components/Home/TeamSection/TeamSection";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
            <Banner></Banner>
            <Features></Features>
            <AboutUs></AboutUs>
+           <TeamSection></TeamSection>
            <Reviews></Reviews>
            <NewsLetter></NewsLetter>
         </div>
---
+++ src/Pages/TrainerBooking/TrainerBooking.jsx
@@ -0,0 +1,10 @@
+
+const TrainerBooking = () => {
+    return (
+        <div>
+            <h2>booked</h2>
+        </div>
+    );
+};
+
+export default TrainerBooking;
